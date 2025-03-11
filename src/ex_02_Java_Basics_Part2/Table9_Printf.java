ee9d12432d9e43caad8da05952982b6691f66811
varshalambe <varshalambe73@gmail.com>
2025-03-11 22:17:06 +0530

    Table 9 Using printf

---
+++ src/ex_02_Java_Basics_Part2/Table9_Printf.java
@@ -0,0 +1,28 @@
+package ex_02_Java_Basics_Part2;
+
+public class Table9_Printf {
+
+    public static void main(String[] args) {
+        int a=9;
+        System.out.printf("%d*1",a);
+        System.out.println();
+        System.out.printf("%d*2",a);
+        System.out.println();
+        System.out.printf("%d*3",a);
+        System.out.println();
+        System.out.printf("%d*4",a);
+        System.out.println();
+        System.out.printf("%d*5",a);
+        System.out.println();
+        System.out.printf("%d*6",a);
+        System.out.println();
+        System.out.printf("%d*7",a);
+        System.out.println();
+        System.out.printf("%d*8",a);
+        System.out.println();
+        System.out.printf("%d*9",a);
+        System.out.println();
+        System.out.printf("%d*10",a);
+
+    }
+}
