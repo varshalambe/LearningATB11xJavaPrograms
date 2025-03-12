908a99611723b3524c5f63095574b91561343342
varshalambe <varshalambe73@gmail.com>
2025-03-12 22:23:23 +0530

    MathFunctions

---
+++ src/ex_03_Java_Basics_Part3/math/mathfunctions.java
@@ -0,0 +1,9 @@
+package ex_03_Java_Basics_Part3.math;
+public class mathfunctions {
+
+    public static void main(String[] args) {
+      int x=10,y=10,z=10 ;
+      System.out.println(Math.cbrt((Math.pow(x,2))+(Math.pow(y,2))-Math.abs(z)));
+
+    }
+}
